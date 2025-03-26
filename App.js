f25f56e4f76d44cb2fccec5cf8fa9eaff3b6f1a0
Santoshi <161740552+SantoshiRB00100@users.noreply.github.com>
2025-03-26 20:16:05 +0530

    Add files via upload
    Signed-off-by: Santoshi <161740552+SantoshiRB00100@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const options = {
+        strings:["Web Developer", "Frontend Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    };
+
+    new Typed(".text", options);
+});
+
+
+
